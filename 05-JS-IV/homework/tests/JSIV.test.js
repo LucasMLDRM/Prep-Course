89979d4ae94db4c722e6e5f58bd64aea38a364a4
LucasMLDRM <lucasgenius13@gmail.com>
2022-07-01 19:43:27 -0300

    'lol'

--- 05-JS-IV/homework/tests/JSIV.test.js
+++ 05-JS-IV/homework/tests/JSIV.test.js
@@ -4,7 +4,7 @@ const {
   agregarPropiedad,
   invocarMetodo,
   multiplicarNumeroDesconocidoPorCinco,
-  eliminarPropiedadlol,
+  eliminarPropiedad,
   nuevoUsuario,
   tieneEmail,
   tienePropiedad,
